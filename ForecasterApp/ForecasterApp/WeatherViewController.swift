6d3466ca376b11e59649182ae19460e6b5dc0e6c
josmoss <josmoss@gmail.com>
2016-06-29 14:54:59 -0600

    Created WeatherViewController.

---
+++ ForecasterApp/ForecasterApp/WeatherViewController.swift
@@ -0,0 +1,25 @@
+//
+//  WeatherViewController.swift
+//  ForecasterApp
+//
+//  Created by Joe Moss on 6/29/16.
+//  Copyright © 2016 Iron Yard_Joe Moss. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+    }
+
+
+}
